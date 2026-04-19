baf43a3daef1093484e662dbb3c4cc88c8b6cefd
v_iHuGi <hugoooazevedo@gmail.com>
2026-04-19 14:10:40 +0100

    Initial Commit

---
+++ src/main.rs
@@ -0,0 +1,41 @@
+use std::fs;
+use std::io::Error;
+
+fn main() {
+    // let text = fs::read_to_string("logs.txt");
+
+    //println!("{:#?}", text);
+
+    match divide(5.0, 3.0) {
+        Ok(result_of_division) => {
+            println!("{}", result_of_division);
+        }
+        Err(what_went_wrong) => {
+            println!("{}", what_went_wrong);
+        }
+    }
+
+    match validade_email(String::from("hugo@email.com")) {
+        Ok(..) => println!("Email is valid"),
+        Err(reason_this_failed_validation) => {
+            println!("{}", reason_this_failed_validation)
+        }
+    }
+}
+
+fn validade_email(email: String) -> Result<(), Error> {
+    if email.contains("@") {
+        // Success
+        Ok(())
+    } else {
+        Err(Error::other("Emails must have an @"))
+    }
+}
+
+fn divide(a: f64, b: f64) -> Result<f64, Error> {
+    if b == 0.0 {
+        Err(Error::other("Can't divide by Zero"))
+    } else {
+        Ok(a / b)
+    }
+}
